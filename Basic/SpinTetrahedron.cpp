103ce3ce0041408b8b19e9314452bb8a24643a0f
ACODHW <66577471+Acodhw@users.noreply.github.com>
2023-09-12 15:46:53 +0900

    Update SpinTetrahedron.cpp

--- Basic/SpinTetrahedron.cpp
+++ Basic/SpinTetrahedron.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <cmath>
-#include "include/GL/freeglut.h"
+#include "freeglut.h"
 
-int width = 600, height = 400; // Window Width and Height
+int width = 640, height = 360; // Window Width and Height
 
-void rendering(); 
+void rendering();
 void DoTimer(int value);
 void reShape(int w, int h); // For Rendering Functions
 
@@ -19,8 +19,8 @@ double basePoint4[3] = { 0, (2 * sqrt(3)), -(sqrt(6) * 0.5) }; // Tetrahedron Ba
 
 
 int main(int args, char** argv) {
-	
-	glutInit(&args, argv); // Start with openGL
+
+	glutInit(&args, argv); // Start with openGL-freeglut
 	glutInitWindowSize(width, height); // Window size set
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB); // Display Mode Set with RGB and Double Buffer
 	glutCreateWindow("First");
@@ -41,7 +41,7 @@ void rendering() {
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(-12, 12, -8, 8, -8, 8); // Set Displaying Space
+	glOrtho(-16, 16, -9, 9, -8, 8); // Set Displaying Space
 
 	glMatrixMode(GL_MODELVIEW_STACK_DEPTH); // Set Modeling Coordinate System
 
@@ -50,35 +50,56 @@ void rendering() {
 	double point2[3] = { basePoint2[0] * (cos(angle)) + basePoint2[2] * (-sin(angle)), basePoint2[1], basePoint2[0] * (sin(angle)) + basePoint2[2] * (cos(angle)) };
 	double point3[3] = { basePoint3[0] * (cos(angle)) + basePoint3[2] * (-sin(angle)), basePoint3[1], basePoint3[0] * (sin(angle)) + basePoint3[2] * (cos(angle)) };
 	double point4[3] = { basePoint4[0] * (cos(angle)) + basePoint4[2] * (-sin(angle)), basePoint4[1], basePoint4[0] * (sin(angle)) + basePoint4[2] * (cos(angle)) }; // Point Rotate
-	
+
 	glMatrixMode(GL_MODELVIEW); // Set Modeling Coordinate System
 
 	glBegin(GL_TRIANGLES); // Tetrahedron Draw
-		glColor3f(1, 0, 0);
-		glVertex3f(point1[0], point1[1], point1[2]);		
-		glVertex3f(point4[0], point4[1], point4[2]);
-		glColor3f(0.45f, 0, 0);	
-		glVertex3f(point3[0], point3[1], point3[2]);
+	glColor3d(1, 0, 0);
+	glVertex3d(point1[0], point1[1], point1[2]);
+	glVertex3d(point4[0], point4[1], point4[2]);
+	glColor3d(0.45, 0, 0);
+	glVertex3d(point3[0], point3[1], point3[2]);
 
-		glColor3f(0, 1, 0);
-		glVertex3f(point1[0], point1[1], point1[2]);		
-		glVertex3f(point4[0], point4[1], point4[2]); 
-		glColor3f(0, 0.45f, 0);
-		glVertex3f(point2[0], point2[1], point2[2]);
+	glColor3d(0, 1, 0);
+	glVertex3d(point1[0], point1[1], point1[2]);
+	glVertex3d(point4[0], point4[1], point4[2]);
+	glColor3d(0, 0.45, 0);
+	glVertex3d(point2[0], point2[1], point2[2]);
 
-		glColor3f(0, 0, 1);
-		glVertex3f(point1[0], point1[1], point1[2]);
-		glColor3f(0, 0, 0.45f);
-		glVertex3f(point2[0], point2[1], point2[2]);
-		glVertex3f(point3[0], point3[1], point3[2]);
+	glColor3d(0, 0, 1);
+	glVertex3d(point1[0], point1[1], point1[2]);
+	glColor3d(0, 0, 0.45);
+	glVertex3d(point2[0], point2[1], point2[2]);
+	glVertex3d(point3[0], point3[1], point3[2]);
 
-		glColor3f(0.3f, 0.3f, 0);
-		glVertex3f(point2[0], point2[1], point2[2]);		
-		glVertex3f(point3[0], point3[1], point3[2]);
-		glColor3f(1, 1, 0);
-		glVertex3f(point4[0], point4[1], point4[2]);
+	glColor3d(0.3, 0.3, 0);
+	glVertex3d(point2[0], point2[1], point2[2]);
+	glVertex3d(point3[0], point3[1], point3[2]);
+	glColor3d(1, 1, 0);
+	glVertex3d(point4[0], point4[1], point4[2]);
 
 	glEnd();
+
+	glBegin(GL_LINES);
+	glColor3d(1,1,1);
+	glVertex3d(-16,0,0);
+	glVertex3d(16, 0, 0);
+
+	glVertex3d(0, -9, 0);
+	glVertex3d(0, 9, 0);
+
+	glColor3d(0.5, 0.5, 0.5);
+	for (int i = -9; i < 9; i++) {
+		glVertex3d(-16, i, 0);
+		glVertex3d(16, i, 0);
+	}
+	for (int i = -16; i < 16; i++) {
+		glVertex3d(i, -9, 0);
+		glVertex3d(i, 9, 0);
+	}
+
+	glEnd();
+
 	glutSwapBuffers(); // Buffer Swap
 }
 
@@ -90,8 +111,13 @@ void DoTimer(int value) {
 	glutTimerFunc(1, DoTimer, 1); // Repeat
 }
 
-void reShape(int w, int h){
-	glViewport(0, 0, w, h); // Change Viewport
+void reShape(int w, int h) {
+	if (16 * h > 9 * w) {
+		glViewport(0, (h  - ((w * 9) / 16))/2, w, (((w * 9) / 16))); // Change Viewport
+	}
+	else {
+		glViewport((w  - ((h * 16) / 9))/2, 0, ((h * 16) / 9), h); // Change Viewport
+	}	
 	width = w;
 	height = h;
 }
